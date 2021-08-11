15e1da342f9e19a0ed3ab842c87306103e331a12
Eudes Roger <eudesroger7@gmail.com>
2021-08-11 19:32:11 -0300

    init project page main - banner, list and request - wip input search component empty state component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { InputSearchComponent } from './components/input-search/input-search.component';
+import {NzIconModule} from 'ng-zorro-antd/icon';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import {CommonModule, registerLocaleData} from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EmptyStateComponent } from './components/empty-state/empty-state.component';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    InputSearchComponent,
+    EmptyStateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzIconModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    CommonModule
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
